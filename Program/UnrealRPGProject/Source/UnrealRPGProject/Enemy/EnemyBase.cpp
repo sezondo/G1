343c56d1c380f0cc83d4aadbbc82b282da9914cd
sezondo <psdd205050@gmail.com>
2025-07-29 23:49:39 +0900

    cpp 파일 정의 추가 base 완성

--- Program/UnrealRPGProject/Source/UnrealRPGProject/Enemy/EnemyBase.cpp
+++ Program/UnrealRPGProject/Source/UnrealRPGProject/Enemy/EnemyBase.cpp
@@ -8,7 +8,10 @@ AEnemyBase::AEnemyBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	MaxHP = 100.f;
+	CurrentHP = MaxHP;
+	CurrentState = EEnemyState::Idle;
+	TargetActor = nullptr;
 }
 
 // Called when the game starts or when spawned
@@ -22,8 +25,54 @@ void AEnemyBase::BeginPlay()
 void AEnemyBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	UpdateState(DeltaTime);
 }
+
+void AEnemyBase::UpdateState(float DeltaTime)
+{
+    switch (CurrentState)
+    {
+    case EEnemyState::Idle:
+        HandleIdle();
+        break;
+    case EEnemyState::Patrol:
+        HandlePatrol();
+        break;
+    case EEnemyState::Chase:
+        HandleChase();
+        break;
+    case EEnemyState::Attack:
+        HandleAttack();
+        break;
+    case EEnemyState::Hurt:
+        HandleHurt();
+        break;
+    case EEnemyState::Die:
+        HandleDie();
+        break;
+    default:
+        break;
+    }
+}
+
+void AEnemyBase::ChangeState(EEnemyState NewState) {
+    if (CurrentState == NewState) return;
+
+    // 상태변경 확인
+    UE_LOG(LogTemp, Log, TEXT("Enemy State Changed: %d -> %d"), (int32)CurrentState, (int32)NewState);
+
+    CurrentState = NewState;
+}
+
+//오버라이딩 용
+void AEnemyBase::HandleIdle() {}
+void AEnemyBase::HandlePatrol() {}
+void AEnemyBase::HandleChase() {}
+void AEnemyBase::HandleAttack() {}
+void AEnemyBase::HandleHurt() {}
+void AEnemyBase::HandleDie() {}
+
+
 
 
 
